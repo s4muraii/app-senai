d949a62cf981a6d5fed163f9ba1ca203b5d66cb3
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:36:07 -0300

    up

--- AppSenai/components/item.tsx
+++ AppSenai/components/item.tsx
@@ -1,42 +1,48 @@
-import React from "react";
-import { Image, Text, View, StyleSheet } from "react-native";
+    import React from "react";
+    import { Image, Text, View, StyleSheet,  } from "react-native";
+    import useTheme from "../temas/temas";
+    import { TouchableOpacity } from "react-native-gesture-handler";
+    import { Link } from "expo-router";
 
 type ItensProps = {
-id: string,
-item: string,
+    id: string,
+    item: string,
+    href : string,
 }
 
-const Itens = ({id, item }: ItensProps) => {
-    return(
-        <View style={styles.display}>
-            <Text style={styles.text}>{id}</Text>
-            <Text style={styles.item}>{item}</Text>
-        </View>
-    );
-};
+    const Itens = ({id, item, href}: ItensProps) => {
+        const cores = useTheme();
+        const styles = StyleSheet.create({
+        display: {
+            borderBottomColor: cores.bgPrimaryVariant,
+            borderBottomWidth: 2,
+        },
+        text : {
+            fontSize: 20,
+            fontWeight: '500',
+            color: cores.textColor,
+            alignContent: 'center',
+            marginTop: 20,
 
-export default Itens;
+            marginLeft: 25,
+        },
+        item: {
+            fontSize: 15,
+            fontWeight: '500',
+            color: 'lightgray',
+            alignContent: 'center',
+            marginLeft: 25,
+            marginBottom: 20,
+        },
+    });
+        return(
+        <Link href={href} asChild>
+            <TouchableOpacity style={styles.display}>
+                <Text style={styles.text}>{id}</Text>
+                <Text style={styles.item}>{item}</Text>
+            </TouchableOpacity>
+        </Link>
+        );
+    };
 
-const styles = StyleSheet.create({
-    display: {
-        borderBottomColor: '#f5f5f5',
-        borderBottomWidth: 2,
-    },
-    text : {
-        fontSize: 20,
-        fontWeight: '500',
-        color: 'black',
-        alignContent: 'center',
-        marginTop: 20,
-
-        marginLeft: 25,
-    },
-    item: {
-        fontSize: 15,
-        fontWeight: '500',
-        color: 'lightgray',
-        alignContent: 'center',
-        marginLeft: 25,
-        marginBottom: 20,
-    },
-});
+    export default Itens;
