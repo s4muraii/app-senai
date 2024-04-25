ace9cea7a7a56b9e011ae5ec8b6db288c9ba5d50
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:41:08 -0300

    inserção do dark mode

--- AppSenai/app/index.tsx
+++ AppSenai/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, useColorScheme, StatusBar} from "react-native";
 import Logo from "../components/logo";
 import InputField from "../components/input";
 import Botao from "../components/button";
@@ -10,51 +10,15 @@ function login() {
 
   const cores = useTheme();
 
-  return (
-    <View>
-    <View style={
-      {
-        backgroundColor: cores.bgPrimary,
-        height: '100%',
-      }
-    
-    }>
-      <View>
-        <Logo text="Login" color={cores.bgPrimary} />
-      </View>
-      <View>
-        <Text style={styles.text}>Faça Login Para Acessar o Sistema!</Text>
+  const Theme = useColorScheme();
+  const backgroundColor = Theme === 'dark' ? cores.bgPrimary : '#000000';
+  const corbotoes = Theme === 'dark' ? cores.bgSecondary : '#000000';
 
-        <InputField label="Email:" placeholder="Digite Email" secureTextEntry={false} />
-        <InputField label="Senha:" placeholder="Digite sua Senha" secureTextEntry={true} />
-        <Link href="./drawer/home/index" asChild>
-          <Botao text="Entrar" color={cores.bgSecondary} href="./drawer/home/itens"/>
-        </Link>
-      </View>
-      <View>
-        <View style={styles.links}>
-        <Link href="/cadastro" asChild>
-          <TouchableOpacity>
-            <Text style={[styles.link, {color: cores.textColor}]}>Cadastre-Se</Text>
-          </TouchableOpacity>
-        </Link>
-        <Link href="/rec" asChild>
-          <TouchableOpacity>
-            <Text style={[styles.link, {color: cores.textColor}]}>Esqueceu sua senha?</Text>
-          </TouchableOpacity>
-        </Link>
-        </View>
-      </View>
-    </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: '500',
-    color: 'black',
+    color: cores.textColor,
     alignContent: 'center',
     textAlign: 'center',
     marginBottom: 20,
@@ -69,5 +33,47 @@ const styles = StyleSheet.create({
     margin: 20,
   },
 });
+
+  return (
+    <View>
+        <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+    <View style={
+      {
+        backgroundColor: cores.bgPrimary,
+        height: '100%',
+      }
+    
+    }>
+      <View>
+        <Logo text="Login " color={backgroundColor} />
+      </View>
+      <View>
+        <Text style={styles.text}>Faça Login Para Acessar o Sistema!</Text>
+
+        <InputField label="Email:" placeholder="Digite Email" secureTextEntry={false} />
+        <InputField label="Senha:" placeholder="Digite sua Senha" secureTextEntry={true} />
+        <Link href="./drawer/home/index" asChild>
+          <Botao text="Entrar" color={corbotoes} href="./drawer/home/itens"/>
+        </Link>
+      </View>
+      <View>
+        <View style={styles.links}>
+        <Link href="/cadastro" asChild>
+          <TouchableOpacity>
+            <Text style={[styles.link, {color: cores.textColor}]}>Cadastre-Se</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link href="/rec" asChild>
+          <TouchableOpacity>
+            <Text style={[styles.link, {color: cores.textColor}]}>Esqueceu sua senha? </Text>
+          </TouchableOpacity>
+        </Link>
+        </View>
+      </View>
+    </View>
+    </View>
+  );
+}
+
 
 export default login;
