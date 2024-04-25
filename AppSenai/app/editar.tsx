d33135c6c712d1b65def37f414e2c5c41214d508
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:41:30 -0300

    inserção do dark mode

--- AppSenai/app/editar.tsx
+++ AppSenai/app/editar.tsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import Header from "../components/header";
 import InventoryInput from "../components/invinput";
 import Bottom from "../components/bottom";
+import useTheme from "../temas/temas";
 export default function AddItens() {
+    const cores = useTheme();
         const styles = StyleSheet.create({
     paddingtop: {
         paddingTop: 60,
     }});
 
     return (
-        <View>
+        <View style={
+            {
+                backgroundColor: cores.bgPrimary,
+                height: '100%',
+            }
+        }>
+        <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
 <View>
-    <Header cor="#ff0000" texto="Adicionar Itens" />
     <View style={styles.paddingtop}>
-    <InventoryInput label='n inventario'/>
+    <InventoryInput label='N inventario'/>
     <InventoryInput label='Descrição' />
     <InventoryInput label='Local'/>
     <InventoryInput label='Responsavel'/>
     <InventoryInput label='Data de registro'/>
     </View>
-    <Bottom color="ffff" coloricons= "#595959" bottom={0} position="fixed" />
     </View>
 </View>
     );
