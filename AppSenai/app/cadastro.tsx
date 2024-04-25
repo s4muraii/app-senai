b28d04455c4dd85aa9aa637d2a9ebf9200b5b539
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:42:00 -0300

    inserção do dark mode

--- AppSenai/app/cadastro.tsx
+++ AppSenai/app/cadastro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, useColorScheme } from 'react-native';
+import { View, useColorScheme, StatusBar } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Logo from '../components/logo';
 import InputField from '../components/input';
@@ -8,6 +8,9 @@ import useTheme from '../temas/temas';
 
 export default function Cadastro() {
   const cores = useTheme();
+    const Theme = useColorScheme();
+    const backgroundColor = Theme === 'dark' ? cores.bgPrimary : '#FF0000';
+    const corbotoes = Theme === 'dark' ? cores.bgSecondary : '#000000';
   return (
 
     <KeyboardAwareScrollView
@@ -18,14 +21,15 @@ export default function Cadastro() {
       <View style={
         {backgroundColor: cores.bgPrimary, height: '100%'}
       }>
-        <Logo text="Cadastre-se" color={cores.bgPrimary} />
+        <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+        <Logo text="Cadastre-se" color={backgroundColor} />
         <InputField label="Nome" placeholder="Digite seu nome" secureTextEntry={false} />
         <InputField label="Sobrenome" placeholder="Digite seu Sobrenome" secureTextEntry={false} />
         <InputField label="Email" placeholder="Digite seu Email" secureTextEntry={false} />
         <InputField label="Telefone" placeholder="Digite seu Telefone" secureTextEntry={false} />
         <InputField label="Senha" placeholder="Digite sua Senha" secureTextEntry={true} />
         <InputField label="Confirme sua Senha" placeholder="Confirme sua Senha" secureTextEntry={true}/>
-        <Botao text="Cadastrar" color={cores.bgSecondary  } href='/index' />
+        <Botao text="Cadastrar" color={corbotoes} href='.' />
       </View>
     </KeyboardAwareScrollView>
   );
