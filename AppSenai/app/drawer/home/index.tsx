dad4cd144e203127ad73599787a46d555ae0cda0
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:44:19 -0300

    estruturação, insercão do dark mode, inserção da expo camera e correções na mesma

--- AppSenai/app/drawer/home/index.tsx
+++ AppSenai/app/drawer/home/index.tsx
@@ -1,24 +1,73 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, StatusBar, TouchableOpacity } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
-
+import { Camera } from 'expo-camera';
+import useTheme from "../../../temas/temas";
 
 export default function Scanner() {
-
+    const [openCamera, setOpenCamera] = useState(false);
+    const [hasPermission, setHasPermission] = useState(null);
+    const cores = useTheme();
     const styles = StyleSheet.create({
         displaycode: {
             justifyContent: "center",
             alignItems: "center",
-            paddingTop: 300,
-            backgroundColor: "#FBFBFB",
+            backgroundColor: cores.bgPrimary,
+            height: "100%",
+        },
+        closeButton: {
+            position: 'absolute',
+            top: 40,
+            right: 20,
         },
     });
 
+    useEffect(() => {
+    (async () => {
+        try {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        } catch (e) {
+            console.log(e);
+        }
+    })();
+}, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+        if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return (
+            <>
+                <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+                <Text>Sem acesso a camera!</Text>
+            </>
+        );
+    }
+
+    if (openCamera) {
+        return (
+            <View style={{ flex: 1 }}>
+                <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+                <Camera style={{ flex: 1 }} />
+                <TouchableOpacity style={styles.closeButton} onPress={() => setOpenCamera(false)}>
+                    <MaterialIcons name="close" size={30} color={cores.textColor} />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
     return(
-    <View style={styles.displaycode}>
-        <MaterialIcons name="qr-code-scanner" size={300} color="#5d5c5c" />
-    </View>
-
-
-);
-} 
+        <>
+            <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+            <View style={styles.displaycode}>
+                <TouchableOpacity onPress={() => setOpenCamera(true)}>
+                    <MaterialIcons name="qr-code-scanner" size={300} color={cores.textColor} />
+                </TouchableOpacity>
+            </View>
+        </>
+    )
+}
