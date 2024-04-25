c9baaad6d69e286df88e8a3dab46362e62597137
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:40:40 -0300

    inserção do dark mode

--- AppSenai/app/infoitens.tsx
+++ AppSenai/app/infoitens.tsx
@@ -1,35 +1,38 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import Header from "../components/header";
-import ProdutoInfo from "../components/produtinfo";
-import Bottom from "../components/bottom";
+import { View, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
+import ProdutoInfo from "../components/produtoinfo";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import useTheme from "../temas/temas";
+import { Link } from "expo-router";
+
 export default function InfoItens() {
+    const cores = useTheme();
     const styles = StyleSheet.create({
         paddingtop: {
-            paddingTop: 60,
-            backgroundColor: "#FBFBFB",
+            backgroundColor: cores.bgPrimary,
         },
-            addButtonContainer: {
-    position: "absolute",
-    bottom: -250,
-    right: 0,
-    margin: 20,
-    marginBottom: 40,
-    marginRight: 20,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-},
-        },);
+        addButtonContainer: {
+            position: "absolute",
+            bottom: 20,
+            right: 0,
+            margin: 20,
+            marginBottom: 40,
+            marginRight: 20,
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: cores.bgPrimary,
+            alignItems: "center",
+            justifyContent: "center",
+        },
+    });
     return(
-        <View>
-            <View>
-                <Header cor="#ff0000" texto="Informações do Item" />
-            </View>
+        <View style={{
+            paddingTop: 150,
+            backgroundColor: cores.bgPrimary,
+            height: "100%",
+        }}>
+            <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
             <View style={styles.paddingtop}>
                 <ProdutoInfo label='N inventario' info='956389'/>
                 <ProdutoInfo label='Descrição' info='CADEIRA  SENAI'/>
@@ -38,10 +41,11 @@ export default function InfoItens() {
                 <ProdutoInfo label='Data de registro' info='12/12/2021'/>
             </View>
             <View style={styles.addButtonContainer}>
-                <MaterialCommunityIcons name="pencil-circle" size={60} color="red" />
-            </View>
-            <View>
-                <Bottom color="ffff" coloricons= "#595959" position="fixed" bottom={-325}/>
+                <Link href="/editar" asChild>
+                    <TouchableOpacity>
+                        <MaterialCommunityIcons name="pencil-circle" size={60} color="red" />
+                    </TouchableOpacity>
+                </Link>
             </View>
         </View>
     )
