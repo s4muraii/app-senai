1edeb896ded5c84638575a3c3fcb273361d2f989
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:42:20 -0300

    inserção do dark mode

--- AppSenai/app/additens.tsx
+++ AppSenai/app/additens.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet,useColorScheme } from "react-native";
+import { View, Text, StyleSheet,useColorScheme, StatusBar } from "react-native";
+import Header from "../components/header";
 import InventoryInput from "../components/invinput";
+import Bottom from "../components/bottom";
+import { AntDesign } from '@expo/vector-icons';
 import useTheme from "../temas/temas";
 export default function AddItens() {
     const cores = useTheme();
@@ -10,10 +13,12 @@ export default function AddItens() {
     }});
 
     return (
+        
         <View style={{
             backgroundColor: cores.bgPrimary,
             height: "100%",
         }}>
+        <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
 <View>
     <View style={styles.paddingtop}>
     <InventoryInput label='N inventario'/>
