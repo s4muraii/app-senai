d57c52920cdcca266aa8ebef0e7a67615faa57b0
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:39:52 -0300

    atualizações e correções

--- AppSenai/app/load.tsx
+++ AppSenai/app/load.tsx
@@ -1,25 +1,33 @@
+import { Image, View, StyleSheet, StatusBar } from "react-native";
+import { FontAwesome6 } from '@expo/vector-icons';
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
-import { FontAwesome6 } from '@expo/vector-icons'; // Add the import statement for FontAwesome6
 
 const Load = () => {
     const styles = StyleSheet.create({
+
         Load: {
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
             backgroundColor: '#FF0000',
         },
+
         Logo: {
             width: 181,
             height: 39,
         },
+
     });
 
     return (
         <View style={styles.Load}>
-            <Image source={require('../components/img/Logo.png')} style={styles.Logo} />
-            <FontAwesome6 name="circle-notch" size={60} color="white" style={{ marginTop: 80 }} />
+            <Image source={require('../assets/Logo.png')} style={styles.Logo} />
+
+            <FontAwesome6 name="circle-notch" 
+            size={60} 
+            color="white" 
+            style={{ marginTop: 80 }} 
+            />
         </View>
     );
 }
