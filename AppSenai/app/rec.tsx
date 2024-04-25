309ffb623cd7f2c615a0a3cecde24cacf3b22926
Vinicius Moura <104801135+s4muraii@users.noreply.github.com>
2024-04-25 08:39:25 -0300

    inserção do dark mode e correções

--- AppSenai/app/rec.tsx
+++ AppSenai/app/rec.tsx
@@ -2,29 +2,42 @@ import React from "react";
 import Logo from "../components/logo";
 import InputField from "../components/input";
 import Botao from "../components/button";
-import { Text, View, StyleSheet } from "react-native";
+import { Image, Text, View, StyleSheet, StatusBar, useColorScheme } from "react-native";
+import useTheme from "../temas/temas";
+
+
 
 function Recuperar(){
-    return(
-    <View>
-    <View>
-    <Logo text="Recuperção de Senha" color="darkblue" />
-    </View>
-    <Text style={styles.text}>Insira seu e-mail para recuperar a senha!</Text>
-    <InputField  label="Email:" placeholder="Digite seu Email" secureTextEntry={false}/>
-    
-    <Botao text="Recuperar" color="black" href="/rec.tsx" />
-    </View>
-)}
-
-const styles = StyleSheet.create({
+    const cores = useTheme();
+    const Theme = useColorScheme();
+    const backgroundColor = Theme === 'dark' ? cores.bgPrimary : '#011E83';
+    const corbotoes = Theme === 'dark' ? cores.bgSecondary : '#000000';
+    const styles = StyleSheet.create({
     text : {
         fontSize: 20,
         fontWeight: '500',
-        color: 'black',
+        color: cores.textColor,
         alignContent: 'center',
         textAlign: 'center',
         marginBottom: 20,
     },
 });
+    return(
+    <>
+    <StatusBar backgroundColor={cores.bgPrimaryVariant} barStyle="light-content" />
+    <View style={{
+        backgroundColor: cores.bgPrimary,
+        height: '100%',
+    
+    }}>
+    <View>
+    <Logo text="Recuperção de Senha" color={backgroundColor} />
+    </View>
+    <Text style={styles.text}>Insira seu e-mail para recuperar a senha!</Text>
+    <InputField  label="Email:" placeholder="Digite seu Email" secureTextEntry={false}/>
+    <Botao text="Recuperar " color={corbotoes} href="." />
+    </View>
+    </>
+)}
+
 export default Recuperar;
